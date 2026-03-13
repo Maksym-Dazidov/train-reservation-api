19cc45e0d680878e8abf549a867689bbae5ea87a
Maksym Dazidov <maksymdazidov@gmail.com>
2026-03-13 05:25:09 +0100

    feat: add API routing for all ViewSets

---
+++ railway/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    TrainTypeViewSet,
+    StationViewSet,
+    CrewViewSet,
+    TrainViewSet,
+    RouteViewSet,
+    JourneyViewSet,
+    TicketViewSet,
+    OrderViewSet
+)
+
+router = DefaultRouter()
+
+router.register('train-types', TrainTypeViewSet, basename='train-type')
+router.register('stations', StationViewSet, basename='station')
+router.register('crews', CrewViewSet, basename='crew')
+router.register('trains', TrainViewSet, basename='train')
+router.register('routes', RouteViewSet, basename='route')
+router.register('journeys', JourneyViewSet, basename='journey')
+router.register('tickets', TicketViewSet, basename='ticket')
+router.register('orders', OrderViewSet, basename='order')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+app_name = 'railway'
